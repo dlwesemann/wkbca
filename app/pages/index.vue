e5b0e2a4146cad2bffbfd6c1db5ef5f00012fab1
Darren Wesemann <darren.wesemann@gmail.com>
2022-03-17 20:45:58 -0600

    Home page button exploration
    Attempt to modify the home page Sign Up button, change to password, initial exploration

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -20,15 +20,15 @@
               class="appearance-none mb-36 bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
               type="text"
               name="email"
-              placeholder="your@email.com"
-              aria-label="Email address"
+              placeholder="HOA Password"
+              aria-label="HOA Password"
             />
 
             <button
               class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
               type="submit"
             >
-              Sign Up
+              Password
             </button>
           </form>
         </div>
@@ -75,8 +75,9 @@ export default class Home extends Vue {
 
   validEmail(email): boolean {
     // eslint-disable-next-line
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+    //const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    //return re.test(email);
+    return true;
   }
 
   async handleSubmit(): Promise<void> {
