12de056298194ac4757917b4d22f1f78a539378d
Darren Wesemann <darren.wesemann@gmail.com>
2022-04-13 22:51:46 -0600

    blog to announcements

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="blog">
     <div class="py-8 md:py-16 text-center">
-      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Blog</h1>
+      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Announcements</h1>
       <h2 class="text-base md:text-lg lg:text-xl xl:text-2xl">
-        West Kawela Bay Community Association Posts
+        West Kawela Bay Community Association Announcements and Communications
       </h2>
     </div>
 
