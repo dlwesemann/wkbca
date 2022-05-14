07a1f62d1617b764c7f95b309fee98c3b94e15d0
dlwesemann <101487454+dlwesemann@users.noreply.github.com>
2022-05-14 05:00:08 -0600

    another attempt at root location, content

--- app/pages/library/index.vue
+++ app/pages/library/index.vue
@@ -68,7 +68,7 @@ export default class LibraryIndex extends Vue {
      if (!window.sessionStorage.getItem("isLoggedIn")) {
         window.location.replace("https://wkbca.netlify.app/");
      };
-     window.location.replace("static/test.html");
+     window.location.replace("test.html");
   }
 
   async asyncData({ params, store }) {
